176729d693bd31340ae76a890e5f741f7b47b311
ankitsejwal <ankit@sejw.al>
2018-05-27 10:05:09 +1000

    timestamp and layout changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,31 +1,31 @@
 from flask import Flask, render_template, request
+import time
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', timestamp=time.time())
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', timestamp=time.time())
 
 
 @app.route('/projects')
 def projects():
-    return render_template('projects.html')
+    return render_template('projects.html', timestamp=time.time())
 
 
 @app.route('/careers')
 def careers():
-    return render_template('careers.html')
+    return render_template('careers.html', timestamp=time.time())
 
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
-
+    return render_template('contact.html', timestamp=time.time())
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
