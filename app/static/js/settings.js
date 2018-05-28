c793cd8d30f04a2af26083ef9bf1d9075d403821
ankitsejwal <ankit@sejw.al>
2018-05-29 01:58:02 +1000

    getjson data

---
+++ app/static/js/settings.js
@@ -0,0 +1,18 @@
+var settings = getData();
+
+function getData(){
+    var json = null;
+
+    $.ajax({
+        async: false,
+        url: "../static/js/settings.json",
+        dataType: "json",
+        cache: false,
+        success: function(data){
+            json = data;
+        }
+    });
+    return json;
+}
+
+console.log(settings.credentials.regular_login.username)
